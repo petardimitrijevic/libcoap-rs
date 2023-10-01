6ae304e1f715c4ea51f8eaa807f468e7d82ee1f4
Petar Dimitrijevic <petard@ved.mk>
2023-10-01 13:40:52 +0200

    Implement tryfrom for u32 -> CoapLogLevel

--- libcoap/src/utils.rs
+++ libcoap/src/utils.rs
@@ -17,3 +17,24 @@ pub fn coap_set_log_level(level: CoapLogLevel) {
         libcoap_sys::coap_set_log_level(level as i32);
     }
 }
+
+impl TryFrom<u32> for CoapLogLevel {
+    type Error = ();
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        use CoapLogLevel::*;
+
+        Ok(match value {
+            0 => COAP_LOG_EMERG,
+            1 => COAP_LOG_ALERT,
+            2 => COAP_LOG_CRIT,
+            3 => COAP_LOG_ERR,
+            4 => COAP_LOG_WARN,
+            5 => COAP_LOG_NOTICE,
+            6 => COAP_LOG_INFO,
+            7 => COAP_LOG_DEBUG,
+            8 => COAP_LOG_OSCORE,
+            9.. => COAP_LOG_DTLS_BASE,
+        })
+    }
+}
